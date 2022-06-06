31e18e3d38247ceba53fef8f1a36fa1cde4dadc0
Luke Lombardi <lukelombardi@Lukes-MacBook-Pro.local>
2022-06-06 09:56:23 -0400

    add in optional flag to skip referrer checks / caching on readonly volumes

--- pdm/models/candidates.py
+++ pdm/models/candidates.py
@@ -428,6 +428,10 @@ class PreparedCandidate:
         )
 
     def should_cache(self) -> bool:
+        # Optional flag to skip cache
+        if os.getenv("PDM_SKIP_CACHE", "false") == "true":
+            return False
+
         """Determine whether to cache the dependencies and built wheel."""
         link, source_dir = self.ireq.original_link, self.ireq.source_dir
         if self.req.is_vcs and not self.req.editable:
