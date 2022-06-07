5465d8f62189e43bac6bd92309fca50a2b36f98c
Luke Lombardi <lukelombardi@Lukes-MacBook-Pro.local>
2022-06-07 15:12:14 -0400

    Revert "add candidate blacklist"
    This reverts commit 8d62309c39d531dbef6bd870a2be82615a6dc951.

--- pdm/installers/synchronizers.py
+++ pdm/installers/synchronizers.py
@@ -109,8 +109,6 @@ class Synchronizer:
         self.working_set = environment.get_working_set()
         self.ui = environment.project.core.ui
 
-        self.CANDIDATE_BLACKLIST = ["functools32 3.2.3.post2"]
-
         if isinstance(self.no_editable, Collection):
             keys = self.no_editable
         elif self.no_editable:
@@ -208,12 +206,7 @@ class Synchronizer:
             try:
                 self.manager.install(can)
             except Exception:
-
                 spinner.fail(f"Install {can.format()} failed")
-
-                if can.format() in self.CANDIDATE_BLACKLIST:
-                    return can
-
                 raise
             else:
                 spinner.succeed(f"Install {can.format()} successful")
